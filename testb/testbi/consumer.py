00de916e6660b42a845eeb74490612e56d0e807a
kagwe <kagwepeter07@gmail.com>
2022-04-27 18:34:51 +0300

    'show-score'

--- testb/testbi/consumer.py
+++ testb/testbi/consumer.py
@@ -10,7 +10,9 @@ from punctuator import Punctuator
 p = Punctuator('trainer\INTERSPEECH-T-BRNN.pcl')
 
 ask = []
+number_of_questions = 0
 class ChatConsumer(AsyncConsumer):
+    
     async def websocket_connect(self, event):
         print('connected', event)
 
@@ -23,7 +25,12 @@ class ChatConsumer(AsyncConsumer):
         received_data = json.loads(event['text'])
         msg = received_data.get('message')
         path_y=msg
+        
         check_if = []
+       
+        
+        marking_scheme = []
+        confirm_marks = []
 
         if path_y not in check_if:
         
@@ -107,35 +114,138 @@ class ChatConsumer(AsyncConsumer):
                 quiz = random.choice(questions)
                 key = questions.index(quiz)
                 answer = all_look[key]
+                print('done')
                 #print(quiz)
 
                 # print(answer)
                 # print(questions)
                 # print(all_look)
+                global chat_response
                 chat_response = str("")
  
 
         if msg != '' and 'https://www.youtube.com/' not in msg:
+            
             if len(ask) == 0:
+               
                 chat_response = "Please wath the tutorial to the end."
             else:
-                quiz = random.choice(ask)
-                key_find = ask.index(quiz)
-                answer_choice = rep[key_find]
-                other_choice = random.choice(rep)
-                to_chooce = [answer_choice,other_choice]
-                get_A = random.choice(to_chooce)
-                s_key = to_chooce.index(get_A)
+                with open("resco/score.txt","r",encoding="utf8") as f:#
+                        n_data = f.readlines()
+                        f.close()
+                with open("resco/marks.txt","r",encoding="utf8") as f:#
+                        m_data = f.readlines()
+                        f.close()
+                with open("resco/scorecheck.txt","r",encoding="utf8") as f:#
+                        s_data = f.readlines()
+                        f.close()
 
-                if s_key == 0:
-                    option_a = other_choice
-                    option_b = answer_choice
+                if len(n_data) <= 9:
+                    n_data.append('count')
+                    n_data1 = []
+                    for words1 in n_data:
+                        n_data1.append(words1.strip())                        
+                    with open('resco/score.txt','w') as f:
+                        f.writelines('\n'.join(n_data1))
+                        f.close()
+                    quiz = random.choice(ask)
+                    key_find = ask.index(quiz)
+                    answer_choice = rep[key_find]
+                    other_choice = random.choice(rep)
+                    to_chooce = [answer_choice,other_choice]
+                    get_A = random.choice(to_chooce)
+                    s_key = to_chooce.index(get_A)
+
+                    if s_key == 0:
+                        option_a = other_choice
+                        option_b = answer_choice
+                        s_data.append('B')
+                        s_data1 = []
+                        for words2 in s_data:
+                            s_data1.append(words2.strip())
+                                                
+                        with open('resco/scorecheck.txt','w') as f:
+                            f.writelines('\n'.join(s_data1))
+                            f.close()
+
+                    else:
+                        option_a = answer_choice
+                        option_b = other_choice
+                        s_data.append('A')
+                        s_data1 = []
+                        for words2 in s_data:
+                            s_data1.append(words2.strip())
+                                                
+                        with open('resco/scorecheck.txt','w') as f:
+                            f.writelines('\n'.join(s_data1))
+                            f.close()
+                    resp = "<b>Fill in the blacks. Reply with A or B accoriding to the answer selected</b> <br/>" + str((quiz )+ '<font color="green"> ' + '<br/>'  + 'A: ' + str(option_a) +'<br/>' + "B:" + str(option_b)) + '</font>'
+
+                    chat_response = resp 
+                    if str(msg).lower() == 'a':
+                        m_data.append(msg)
+                        m_data1 = []
+                        for words3 in m_data:
+                            m_data1.append(words3.strip())
+                                                
+                        with open('resco/marks.txt','w') as f:
+                            f.writelines('\n'.join(m_data1))
+                            f.close()
+                        
+                    else:
+                        
+                        m_data.append(msg)
+                        m_data1 = []
+                        for words4 in s_data:
+                            m_data1.append(words4.strip())
+                                                
+                        with open('resco/marks.txt','w') as f:
+                            f.writelines('\n'.join(m_data1))
+                            f.close()
+
                 else:
-                    option_a = answer_choice
-                    option_b = other_choice
-                resp = "<b>Fill in the blacks. Reply with A or B accoriding to the answer selected</b> <br/>" + str((quiz )+ '<font color="green"> ' + '<br/>'  + 'A: ' + str(option_a) +'<br/>' + "B:" + str(option_b)) + '</font>'
-                print(resp)
-                chat_response = resp 
+                    total_marks= 10
+                    marks = 0
+                    print(s_data)
+                    print(m_data)
+                    for ans_ch in s_data:
+                        key2 = s_data.index(ans_ch)
+                        ans_of_q = m_data[key2]
+                        if ans_ch == ans_of_q:
+                            marks +=1
+                            print(ans_ch)
+                            print(ans_of_q)
+                        else:
+                            marks = marks
+                    marks_p = float((marks/total_marks)*100)
+                    if marks_p <=20 :
+                        mes = 'Please Watch the video again 🙂'
+                    elif marks_p >=31 and marks_p < 50 :
+                        mes = 'Nice work 👍'
+                    elif marks_p >=50 and marks_p < 60 :
+                        mes = 'Good work 👏'
+                    elif marks_p >=60 and marks_p < 70 :
+                        mes = 'Great 👏👏'
+                    elif marks_p >=70 and marks_p < 90 :
+                        mes = 'That was amazing 👏👏👏'
+                    else:
+                        mes = 'Excellent 🙌🙌'
+                    you_got = str(marks_p) + '%' + mes
+                    chat_response = "Your score: " + you_got 
+                    n_data2 = []
+                    m_data2 = []
+                    s_data2 = []
+                    with open('resco/score.txt','w') as f:
+                        f.writelines('\n'.join(n_data2))
+                        f.close()
+                    with open('resco/scorecheck.txt','w') as f:
+                        f.writelines('\n'.join(s_data2))
+                        f.close()
+                    with open('resco/marks.txt','w') as f:
+                        f.writelines('\n'.join(m_data2))
+                        f.close()
+
+
 
                 
         if not msg:
